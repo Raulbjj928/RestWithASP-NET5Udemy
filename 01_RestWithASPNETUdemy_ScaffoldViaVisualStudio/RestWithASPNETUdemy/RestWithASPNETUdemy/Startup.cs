46ba74a44bd13a1add4e31b0caf7043c45e1be8e
Raul Furtado <raul.furtado@ambevtech.com.br>
2021-09-26 21:57:20 -0300

    test verbs http

--- 01_RestWithASPNETUdemy_ScaffoldViaVisualStudio/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
+++ 01_RestWithASPNETUdemy_ScaffoldViaVisualStudio/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using RestWithASPNETUdemy.Model.Context;
 using RestWithASPNETUdemy.Services.Implementations;
 
 namespace RestWithASPNETUdemy
@@ -22,6 +24,9 @@ namespace RestWithASPNETUdemy
         {
 
             services.AddControllers();
+
+            var connection = Configuration["MySQLConnection:MySQLConnectionString"];
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
             //Dependency Injection
             services.AddScoped<IPersonService, PersonServiceImplementation>();
 
