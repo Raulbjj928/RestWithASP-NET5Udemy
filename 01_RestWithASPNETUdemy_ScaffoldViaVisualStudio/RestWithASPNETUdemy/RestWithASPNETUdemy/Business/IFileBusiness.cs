c0224f417997559004212c64ddb953412b9e2098
Raul Furtado <raul.furtado@ambevtech.com.br>
2021-10-31 23:04:37 -0300

    Implementation of save methods and file downloads, missing fix

---
+++ 01_RestWithASPNETUdemy_ScaffoldViaVisualStudio/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/IFileBusiness.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using RestWithASPNETUdemy.Data.VO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Business
+{
+    public interface IFileBusiness
+    {
+        public byte[] GetFile(string fileName);
+        public Task<FileDatailVO> SaveFileToDisk(IFormFile file);
+        public Task<List<FileDatailVO>> SaveFilesToDisk(IList<IFormFile> file);
+    }
+}
